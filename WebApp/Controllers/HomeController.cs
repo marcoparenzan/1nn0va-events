990ce788e75698b24cc5b95fb5b6a6540293db2d
Marco Parenzan! <marco.parenzan@live.it>
2019-04-24 12:37:20 +0200

    First commit

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return Redirect(nameof(disatpn19));
+        }
+
+        [Route("Heartbeat")]
+        public IActionResult Heartbeat()
+        {
+            return View();
+        }
+
+        [Route("disatpn19")]
+        public IActionResult disatpn19()
+        {
+            return View();
+        }
+    }
+}
